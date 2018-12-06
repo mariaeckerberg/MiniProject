29534df490d95db364514b02e8928b7c84675bd9
Maria Eckerberg <maria@eckerberg.se>
2018-12-06 11:53:41 +0100

    Add ViewModels.

---
+++ MiniProject/Models/CharactersCreateVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniProject.Models
+{
+    public class CharactersCreateVM
+    {
+        public string Name { get; set; }
+        public string Backstory { get; set; }
+        public int Type { get; set; } //Vilken datatyp för drop-down?
+        public int SpecialTrait { get; set; }   //Vilken datatyp för radio buttons?
+    }
+}
