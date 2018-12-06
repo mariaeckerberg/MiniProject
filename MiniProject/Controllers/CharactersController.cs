5d1ea7f2e0651f3ffa812cc5ab0365b1b650b383
Maria Eckerberg <maria@eckerberg.se>
2018-12-06 15:53:01 +0100

    Add Controller, Create View

---
+++ MiniProject/Controllers/CharactersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MiniProject.Models;
+
+namespace MiniProject.Controllers
+{
+    public class CharactersController : Controller
+    {
+
+        [Route("")]
+        [Route("/characters/index")]
+        public IActionResult Index()
+        {
+            return Content("Tjeeena");
+            //return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CharactersCreateVM viewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(viewModel);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- MiniProject/Models/CharactersCreateVM.cs
+++ MiniProject/Models/CharactersCreateVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace MiniProject.Models
     {
         public string Name { get; set; }
         public string Backstory { get; set; }
-        public int Type { get; set; } //Vilken datatyp för drop-down?
+        public SelectListItem[] TypeItems { get; set; }
         public int SpecialTrait { get; set; }   //Vilken datatyp för radio buttons?
     }
 }
